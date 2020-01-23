6c1ab5c1e3bd00500c141258ccb220f07b6b01ea
AskSensors <42293557+asksensors@users.noreply.github.com>
2020-01-23 11:59:25 +0100

    Create subscribe_esp32.ino

---
+++ subscribe_esp32.ino
@@ -0,0 +1,84 @@
+/*
+ * Description:  Read actuator command from AskSensors over MQTT
+ *  Author: https://asksensors.com, 2020
+ *  github: https://github.com/asksensors
+ */
+ 
+ 
+#include <WiFi.h>
+#include <PubSubClient.h>
+
+//TODO: ESP32 MQTT user config
+const char* ssid = ".............."; // Wifi SSID
+const char* password = ".............."; // Wifi Password
+const char* username = ".............."; // my AskSensors username
+const char* subTopic = "actuator/............../.............."; // actuator/username/apiKeyOut
+
+//AskSensors MQTT config
+const char* mqtt_server = "mqtt.asksensors.com";
+unsigned int mqtt_port = 1883;
+
+WiFiClient askClient;
+PubSubClient client(askClient);
+
+void setup() {
+  Serial.begin(115200);
+  Serial.println("*****************************************************");
+  Serial.println("********** Program Start : Read actuator command from AskSensors over MQTT");
+  
+  Serial.print("********** connecting to WIFI : ");
+  Serial.println(ssid);
+
+  WiFi.begin(ssid, password);
+
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("");
+  Serial.println("->WiFi connected");
+  Serial.println("->IP address: ");
+  Serial.println(WiFi.localIP());
+  
+  client.setServer(mqtt_server, mqtt_port);
+  client.setCallback(callback);
+
+  if (!client.connected()) 
+    reconnect();
+  Serial.println("********** Subscribe to AskSensors MQTT topic");
+  // susbscribe
+  client.subscribe(subTopic);
+}
+
+void loop() {
+  client.loop();
+}
+
+
+void callback(char* topic, byte* payload, unsigned int length) {
+  Serial.print("Command teceived from AskSensors[");
+  Serial.print(topic);
+  Serial.print("] ");
+  for (int i = 0; i < length; i++) {
+    Serial.print((char)payload[i]);
+  }
+  
+  Serial.println("");
+}
+
+void reconnect() {
+  // Loop until we're reconnected
+  while (!client.connected()) {
+    Serial.print("********** Attempting MQTT connection...");
+    // Attempt to connect
+    if (client.connect("ESP32Client", username, "")) {  
+      Serial.println("-> MQTT client connected");
+    } else {
+      Serial.print("failed, rc=");
+      Serial.print(client.state());
+      Serial.println("-> try again in 5 seconds");
+      // Wait 5 seconds before retrying
+      delay(5000);
+    }
+  }
+}
